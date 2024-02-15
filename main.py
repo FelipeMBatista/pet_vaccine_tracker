219420d8ac76c564efddb566fc7e090b234f28cd
FelipeMB <felipembatista08@gmail.com>
2024-02-15 20:18:55 -0300

    solved login problem

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 from datetime import date, timedelta
 from client import Client
 from vaccine import Vaccine
+import sheets
 
-today = date.today()
+# If modifying these scopes, delete the file token.json.
+SCOPES = "https://www.googleapis.com/auth/spreadsheets"
 
-renan = Client(
-    "Renan Hug",
-    55555555,
-    "Jubileu"
-)
-felipe = Client(
-    "Felipe Bat",
-    123123123,
-    "Jaime Jaquez Junior"
-)
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1h-WbK4T6rUvD7eUoIO5tBE3pQjxSxrGiKWLpY4lKuEQ"
+SAMPLE_RANGE_NAME = "2024!A2:F5"
 
-vacina1 = Vaccine(renan, date(2024, 1, 12), 1)
-vacina2 = Vaccine(renan, date(2024, 2, 12), 2)
+def main():
+    google_sheets = sheets.Sheets(SCOPES, SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME)
+    google_sheets.login()
+    google_sheets.read_sheet()
 
-def check_date(vaccine):
-    if vaccine.dose == 1:
-        if today + timedelta(days=7) == vaccine.date + timedelta(days=30):
-            return "primeira dose"
-    elif vaccine.dose == 2:
-        if today + timedelta(days=365) == vaccine.date + timedelta(days=358):
-            return "Segunda dose"
+    # today = date.today()
+    #
+    # renan = Client(
+    #     "Renan Hug",
+    #     55555555,
+    #     "Jubileu"
+    # )
+    # felipe = Client(
+    #     "Felipe Bat",
+    #     123123123,
+    #     "Jaime Jaquez Junior"
+    # )
+    #
+    # vacina1 = Vaccine(renan, date(2024, 1, 12), 1)
+    # vacina2 = Vaccine(renan, date(2024, 2, 12), 2)
+    #
+    # def check_date(vaccine):
+    #     if vaccine.dose == 1:
+    #         if today + timedelta(days=7) == vaccine.date + timedelta(days=30):
+    #             return "primeira dose"
+    #     elif vaccine.dose == 2:
+    #         if today + timedelta(days=365) == vaccine.date + timedelta(days=358):
+    #             return "Segunda dose"
+    #
+    # print(check_date(vacina2))
 
-print(check_date(vacina2))
+if __name__ == "__main__":
+    main()
---
+++ sheets.py
@@ -0,0 +1,56 @@
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+class Sheets():
+    def __init__(self, scopes, sample_spreadsheets_id, sample_range_name):
+        self.scopes = scopes,
+        self.sample_spreadsheets_id = sample_spreadsheets_id,
+        self.sample_range_name = sample_range_name
+        # Authentication
+        self.creds = None
+
+    def login(self):
+        # Checking if the token already exists.
+        if os.path.exists("token.json"):
+            self.creds = Credentials.from_authorized_user_file("token.json", self.scopes)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", self.scopes
+                )
+                self.creds = flow.run_local_server(port=0)
+
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(self.creds.to_json())
+
+    def read_sheet(self):
+        try:
+            service = build("sheets", "v4", credentials=self.creds)
+
+            # Call the Sheets API
+            sheet = service.spreadsheets()
+            result = (
+                sheet.values()
+                .get(spreadsheetId=self.sample_spreadsheets_id, range=self.sample_range_name)
+                .execute()
+            )
+            values = result.get("values", [])
+
+            if not values:
+                print("No data found")
+                return
+
+            print("Column, Column")
+            print(values)
+
+        except HttpError as err:
+            print(err)
