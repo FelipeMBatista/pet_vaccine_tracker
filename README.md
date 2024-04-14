208c6567618278fb36ffa075b6b6f8535b898781
FelipeMB <felipembatista08@gmail.com>
2024-04-14 15:51:20 -0300

    finalizating vaccine tracker model

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Este código tem como objetivo ajudar a monitorar a vacinação dos clientes de 
 
 - Inicie um projeto no Google Cloud API, usando a API do Google Sheets.
 - Configure a tela de proteção OAuth.
-- Crie uma planilha no Google Sheets com as seguintes colunas, respectivamente:
+- Crie uma planilha no Google Sheets com as seguintes colunas, conforme exemplo:
 
-    CLIENTE | PET | DATA VACINA | DOSE | CONTATO
+![sheet_example.png](sheet_example.png)
 
 **Formatos**
 
@@ -25,8 +25,7 @@ Este código tem como objetivo ajudar a monitorar a vacinação dos clientes de 
 
 - Preencha a planilha conforme o cliente vacina seus pets, atentando-se à coluna "Dose", como citado acima, pois irá definir o período para lembrete.
 - Execute o código pelo menos uma vez por dia. Ele fará uma checagem nas datas e definirá um lembrete pelo menos 1 semana antes da data de renovação.
-- O lembrete será enviado por e-mail. Importante saber que o programa está sendo feito para uso INTERNO. Portanto, o e-mail será enviado para você mesmo, profissional ou empresa. O e-mail conterá informações pontuais, como nome do cliente, data da dose anterior, nome do pet, etc., juntamente com um link no formato "wa.me/..." onde, ao clicar, você será direcionado diretamente para o WhatsApp Web na conversa com o cliente (conforme o contato fornecido na planilha), já com a mensagem de lembrete escrita. Basta pressionar "Enter".
-- Portanto, o código é feito para uso interno, pois julguei ser mais acessível o envio de mensagem por WhatsApp ao cliente final do que por e-mail. Caso queira enviar um e-mail ao cliente final, será necessário fazer uma mudança no código.
+- O lembrete será inserido como forma de link para o whatsapp já com a mensagem pronta para o cliente e a coloração indicativa para enviar lembrete.
 
 Este código foi desenvolvido para uso interno em um petshop onde trabalho atualmente. Identifiquei esta necessidade, e como a empresa já tem um software terceiro como sistema principal, não busquei tentar integração, pois demandaria muito tempo e talvez nem seria possível. Portanto, não há controle de vendas nem nada muito automatizado, pois não há controle nem garantia de se o cliente realmente foi fazer a nova dose ou se ela expirou, sendo então necessárias mudanças nas doses do pet.
 
--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import sheets
 
 import urllib.parse
 
-from email.message import Message
-import smtplib
-
 import os
 
 from dotenv import load_dotenv
@@ -19,120 +16,63 @@ SCOPES = [os.getenv("SCOPE")]
 # The ID and page (range) of a sample spreadsheet.
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 RANGE_NAME = os.getenv("RANGE_NAME")
-
-# If you are not going to use Gmail, change the Server
-SMTP_SERVER = "smtp.gmail.com"
-PORT = 587  # For starttls
+PETSHOP_NAME = os.getenv("PETSHOP_NAME")
 
 # Saving the actual date
 TODAY = date.today()
 
-
-def send_email(subject, final_msg):
-    """
-    Function responsible for sending the email. Stores the subject, sender's email,
-    recipient's email and sender's password.
-
-    Make an attempt to connect to the Gmail server, log in and finally send the email.
-
-    :param subject: The subject of the email
-
-    :param final_msg: The final message of the email. Embedded messages, such as whatsapp message will come by parameter
-    """
-
-    msg = Message()
-    msg['Subject'] = subject
-    msg['From'] = os.getenv("EMAIL_FROM")
-    msg['To'] = os.getenv("EMAIL_TO")
-    password = os.getenv("SENDER_PASSWORD")
-
-    msg.add_header('Content-Type', 'text/html')
-    msg.set_payload(final_msg)
-
-    try:
-        server = smtplib.SMTP(SMTP_SERVER, PORT)
-        server.ehlo()  # Can be omitted
-        server.starttls()  # Secure the connection
-        server.login(msg['From'], password)
-        server.sendmail(msg['From'], msg['To'], msg.as_string().encode('utf-8'))
-    except Exception as e:
-        # Print any error messages to stdout
-        print(e)
-    finally:
-        server.quit()
-
-
-def email_message(client, wpp_msg):
-    """
-    Function responsible for formulating the email subject and message.
-    Format the text for your necessary cases, format the date for the local format (in this case Brazil)
-    and create the anchor tag responsible for the link for sending the WhatsApp message to the end customer.
-
-    :param client: The client is the object that contains all information about the client,
-    their pet and information related to vaccines.
-
-    :param wpp_msg: Parameter responsible for receiving the message that will be embedded in the WhatsApp sending link.
-    """
-
-    subject = f"Lembrete de vacinação {client.name.title()}"
-    final_msg = f"""\
-    <p>Cliente: {client.name}</p>
-    <p>Pet: {client.pet_name}</p>
-    <p>Data da última dose: {client.vaccine_date.day} / {client.vaccine_date.month} / {client.vaccine_date.year}</p>
-    <p>Marca da vacina aplicada: {client.brand}</p>
-
-    <a href=wa.me/+55{client.phone}?text={wpp_msg}>CLIQUE AQUI PARA ENVIAR O LEMBRETE</a>
-    """
-
-    send_email(subject, final_msg)
-
-
-def start_the_reminders(clients):
+def start_the_reminders(clients, gsheets):
     """
     Function responsible for starting reminders. She walks through the array of customers checking how many doses their
     pets have and makes the appropriate decision based on the information.
 
     :param clients: Array containing all customers present in the vaccination sheet.
+    :param gsheets: Google Sheets (sheets.py) Object
     """
 
     for client in clients:
         if client.dose == 1:
-            if TODAY + timedelta(days=7) == client.vaccine_date + timedelta(days=30):
+            if TODAY <= (client.vaccine_date + timedelta(days=23)) and TODAY >= (client.vaccine_date + timedelta(days=23, weeks=-1)):
                 """
-                If 7 days from now is equal to 1 month after the first dose...
-                
+                If today is within one week before the final date of the first dose...
+        
                 The idea is that today is one week before the customer's first dose is due.
                 """
 
                 # Message that will be sent to the customer. This message will be embedded in the WhatsApp message link.
-                msg = (f"Bom dia {client.name.split()[0].title()}, tudo bem? Aqui é da RuralPet e estou entrando em "
+                msg = (f"Bom dia {client.name.split()[0].title()}, tudo bem? Aqui é da {PETSHOP_NAME} e estou entrando em "
                        f"contato para lembrar da segunda dose da vacina do seu pet {client.pet_name.title()} que vence "
                        f"dentro de uma semana.\n\nCaso queira marcar a vacina, só nos avisar, ok?")
                 wpp_msg = urllib.parse.quote(msg) # Transforming into a url encoding
-                email_message(client, wpp_msg)
+                wpp_msg_link = f"wa.me/+55{client.phone}?text={wpp_msg}"
+                gsheets.update_cell_value(client.row_number, wpp_msg_link)
+
 
         elif client.dose == 2:
-            if TODAY == client.vaccine_date + timedelta(days=358):
+            if TODAY <= (client.vaccine_date + timedelta(days=358)) and TODAY >= (client.vaccine_date + timedelta(days=358, weeks=-1)):
                 """
-                If today is equal to 358 days (1 week to close 1 year) since the last dose...
+                If today is within one week before the annual revaccination...
 
-                The idea is that today is 1 week before the annual revaccination.
+                The idea is that today is one week before the annual revaccination.
                 """
-                msg = (f"Bom dia {client.name.split()[0].title()}, tudo bem? Aqui é da RuralPet e estou entrando em "
+
+                msg = (f"Bom dia {client.name.split()[0].title()}, tudo bem? Aqui é da {PETSHOP_NAME} e estou entrando em "
                        f"contato para lembrar da renovação da vacina do seu pet {client.pet_name.title()} que vence "
                        f"dentro de uma semana.\n\nCaso queira marcar a vacina, só nos avisar, ok?")
                 wpp_msg = urllib.parse.quote(msg)
-                email_message(client, wpp_msg)
+                wpp_msg_link = f"wa.me/+55{client.phone}?text={wpp_msg}"
+                gsheets.update_cell_value(client.row_number, wpp_msg_link)
         else:
             """
             If the dose value is not identified, a more "Generic" reminder will be sent.
             """
-            msg = (f"Bom dia {client.name.split()[0].title()}, tudo bem? Aqui é da RuralPet e estou entrando em "
+            msg = (f"Bom dia {client.name.split()[0].title()}, tudo bem? Aqui é da {PETSHOP_NAME} e estou entrando em "
                    f"contato para lembrar de verificar a vacinação do seu pet {client.pet_name.title()}."
                    f"Nossas anotações indicam que está quase na data de revacinar."
                    f"\n\nCaso queira marcar a vacina, só nos avisar, ok?")
             wpp_msg = urllib.parse.quote(msg)
-            email_message(client, wpp_msg)
+            wpp_msg_link = f"wa.me/+55{client.phone}?text={wpp_msg}"
+            gsheets.update_cell_value(client.row_number, wpp_msg_link)
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -144,33 +84,38 @@ def main():
     google_sheets.login()
     google_sheets.build()
     values = google_sheets.read_sheet() # Receiving the values from the spreadsheet.
-    print(f"{values}\n\n")
-    google_sheets.yellow_color()
 
-    # clients = []
-    # for row in values[1::]:
-    #     """
-    #     Here we walk through the lines of the spreadsheet. Starting with the second line as the first is the "Header".
-    #
-    #     Its columns will be stored separately so that the values can later be used for client instantiation.
-    #     """
-    #     name = row[0]
-    #     pet_name = row[1]
-    #     vaccine_date = date(int(row[2][6::]), int(row[2][3:5]), int(row[2][:2]))
-    #     dose = int(row[3])
-    #     brand = row[4]
-    #     phone = int(row[5])
-    #     new_client = Client(
-    #         name=name,
-    #         pet_name=pet_name,
-    #         vaccine_date=vaccine_date,
-    #         dose=dose,
-    #         brand=brand,
-    #         phone=phone
-    #     )
-    #     clients.append(new_client)
+    clients = []
+    index = 2 # Start in 1 because i have 2 fixed lines
+    for row in values[2::]:
+        """
+        Here we walk through the lines of the spreadsheet. Starting with the second line as the first is the "Header".
 
-    # start_the_reminders(clients) # Starting reminders...
+        Its columns will be stored separately so that the values can later be used for client instantiation.
+        """
+        # print(row)
+        if not row[0]:  # Verificar se a linha está vazia
+            continue  # Pular para a próxima iteração se a linha estiver vazia
+
+        name = row[0]
+        pet_name = row[1]
+        vaccine_date = date(int(row[2][6::]), int(row[2][3:5]), int(row[2][:2]))
+        dose = int(row[3])
+        brand = row[4]
+        phone = int(row[5])
+        new_client = Client(
+            name=name,
+            pet_name=pet_name,
+            vaccine_date=vaccine_date,
+            dose=dose,
+            brand=brand,
+            phone=phone,
+            row_number=index
+        )
+        clients.append(new_client)
+        index += 1
+
+    start_the_reminders(clients, google_sheets) # Starting reminders...
 
 
 if __name__ == "__main__":
